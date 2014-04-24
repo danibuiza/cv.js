49b97792f2ca32ca31e7f44c67bf272a1faad85e
Daniel Gutierrez Diez <danibuiza@gmail.com>
2014-04-24 17:11:56 +0200

    index html

--- README.md
+++ README.md
@@ -17,11 +17,32 @@ main use cases and data model features:
 
 
 still todo:
+-zoom in the maps
+-add marker with some info in the point center
+- rebrush styles
+- add pdf to download that slides
+- email? links?
+- experience tests?
+- university tests?
 -currently it is completely beta
 -generate json and html properly
 -css to generated content
 -store cv in memory or db
 -retrieve list of cvs
 -search engine
+
+
+
+bugs:
+iexplorer:
+- internet explorer does not load correctly, everything is expanded on loading
+- div with map is visible with a grey background at the loading
+- maps are not shown
+- load html does not work -> load will be deleted
+- somehow works now, probably something to do with the cache
+- hover() method jquery does not work properly on IE? cannot believe it...
+
+chrome:
+-load html does not work -> probably load cannot be used
 
 
--- js/mycv.js
+++ js/mycv.js
@@ -5,7 +5,19 @@
 
 $(function()
 {
-    $(".footer").show().appendTo($(".menu_and_content"));
+
+    var professional_experience = { "Interactive Data" : "IDMS", "Accenture" : "Accenture", "Indra Group" : "Indra", "MGM Group" : "MGM", "Hidrocantabrico, EDP Group" : "EDP" };
+
+
+
+    $(".footer").toggle().appendTo($(".menu_and_content"));
+
+    $(document).find(".photo").click(function()
+    {
+        console.log("photo");
+
+        $("<img src='../resources/foto_dani.PNG'>").appendTo($(".text_preview"));
+    });
 
     $(".title_1").click(function()
     {
@@ -17,45 +29,83 @@ $(function()
         $(this).parent().find(".title_2_content").toggle();
     });
 
-    $(".place").hover(function()
+    $(".professional_experience").mouseout(function()
     {
         console.log($(this).text());
 
-        $(".preview").html("where is " + $(this).text() + "?").addClass("content");
-        ;
+        $("#summary_preview").hide();
+    });
 
-    })
-
-    $(".date").hover(function()
+    $(".professional_experience").mouseover(function()
     {
         console.log($(this).text());
 
-        $(".preview").html("what happened on " + $(this).text() + "?").addClass("content");
-        ;
+        $("#summary_preview").show().html(professional_experience[$(this).text().substr($(this).text().indexOf("- ") + 2)]);
 
-    })
+        console.log(professional_experience[$(this).text().substr($(this).text().indexOf("- ") + 2)]);
+
+    });
+
+    $("#text_preview").click(function()
+    {
+        console.log($(this).text());
+
+        var address = $(this).text();
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ 'address' : address}, function(results, status)
+        {
+
+            if (status == google.maps.GeocoderStatus.OK) {
+                var latitude = results[0].geometry.location.lat();
+                var longitude = results[0].geometry.location.lng();
+                yourStartLatLng = new google.maps.LatLng(latitude, longitude);
+                $('#map_canvas').gmap('destroy');
+
+                $('#map_canvas').gmap({'center' : yourStartLatLng, 'zoom' : 15});
+
+                $("#map_canvas").show().gmap('refresh');
+
+            }
+        });
+    });
+
+    $(".place").mouseout(function()
+    {
+        $("#map_canvas").hide();
+
+    });
+
+    $(".place").mouseover(function()
+    {
+
+        $("#text_preview").html("where is " + $(this).text() + "? click here!").addClass("content");
+    });
+
+    $(".date").mouseover(function()
+    {
+        console.log($(this).text());
+        $("#text_preview").html("what happened on " + $(this).text() + "?").addClass("content");
+
+    });
 
     $(".sentence").hover(function()
     {
         console.log($(this).text());
+        $("#text_preview").html($(this).text()).addClass("content");
 
-        $(".preview").html($(this).text()).addClass("content");
-
-    })
+    });
 
     $(".link").hover(function()
     {
         console.log($(this).text());
+        $("#text_preview").html("do you want to open " + $(this).text() + "?");
 
-        $(".preview").html("do you want to open " + $(this).text() + "?");
-
-    })
+    });
 
     $(".email").hover(function()
     {
         console.log($(this).text());
+        $("#text_preview").html("do you want to send an email to " + $(this).text() + "?");
 
-        $(".preview").html("do you want to send an email to " + $(this).text() + "?");
-
-    })
+    });
 });
